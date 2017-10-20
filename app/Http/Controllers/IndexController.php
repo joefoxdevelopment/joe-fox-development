a89c9d5ea9b067d2821090455ab88894fd8d5491
Joe Fox <joefoxdevelopment@gmail.com>
2017-10-20 10:06:44 +0100

    Add basic laravel project with twig support

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JoeFoxDevelopment\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //
+}
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace JoeFoxDevelopment\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'JoeFoxDevelopment\Events\Event' => [
+            'JoeFoxDevelopment\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
